bf69c7a10e111e8049f21da277c19b516725e8a0
umarshaikh001 <umarshaikh01001@gmail.com>
2026-07-05 13:40:07 +0530

    Add LeetCode 1480 - Running Sum of 1D Array

---
+++ Arrays/1480-Running-Sum-of-1d-Array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+
+      //  int sum =0;
+        int sum =0;
+        int n = nums.length;
+        int ans[] = new int[n];
+
+   for(int i =0;i<n;i++) {
+          sum += nums[i];
+         ans[i] =sum;
+     }
+        return ans;
+        
+    }
+}
